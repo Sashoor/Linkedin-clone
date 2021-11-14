304cb0d3fb2ce896bc5b8bc9a5d34838987056ad
Dev machine <saeedashoor.dev@gmail.com>
2021-11-14 11:35:06 -0800

    Completed Header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from './HeaderOption'
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
   return (
@@ -17,9 +23,13 @@ function Header() {
       </div>
 
       <div className="header__right">
-        {/* next step is to create the header__right component */}
-
-      </div>
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={MessageIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C4D03AQHeULv5ywtdTg/profile-displayphoto-shrink_200_200/0/1558204187133?e=1642636800&v=beta&t=dgnmVIyQa7nLndzee4GFEVFh9Jzx0qdVISxfafedo-w" title="Saeed"/>
+    </div>
     </div>
   );
 }
