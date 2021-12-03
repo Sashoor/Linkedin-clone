fe8e925216c65221f0d75c1c38736cf7806a081a
Dev machine <saeedashoor.dev@gmail.com>
2021-12-03 08:25:16 -0800

    added new section Feed & InputOption component

---
+++ src/Feed.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Feed.css'
+import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+
+function Feed() {
+    return (
+        <div className="feed">
+     <div className="feed__inputContainer">
+         <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title='photo' color="#70B5F9" />
+                </div>
+            </div>       
+        </div>
+    )
+}
+
+export default Feed
