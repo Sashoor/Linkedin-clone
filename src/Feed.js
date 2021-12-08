620f82f8e38387942a3859025af64bbb6ec78352
Dev machine <saeedashoor.dev@gmail.com>
2021-12-08 11:17:13 -0800

    created feed section

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
 import ImageIcon from '@mui/icons-material/Image';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from './Post';
 
 function Feed() {
+const [posts , setPosts] = useState([]);
+
+const sendPost = (e) => {
+    e.preventDefault();
+
+    setPosts()
+
+}
+
+
+
+
     return (
         <div className="feed">
      <div className="feed__inputContainer">
@@ -12,13 +28,25 @@ function Feed() {
                     <CreateIcon />
                     <form>
                         <input type="text" />
-                        <button type="submit">Send</button>
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
-                    <InputOption Icon={ImageIcon} title='photo' color="#70B5F9" />
+                    <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9"/>
+                    <InputOption Icon={VideoLibraryIcon} title='Video' color="#e7a33e" />
+                    <InputOption Icon={EventNoteIcon} title='Event' color="#c0cbcd" />
+                    <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7fc15e" />
+
                 </div>
-            </div>       
+            </div>
+
+            
+            {/* Post */}
+            {posts.map((post) => (
+                <Post />
+            ))}
+            <Post name='Saeed Ashoor' description='This is test' message='WOW this is working' />
+
         </div>
     )
 }
--- src/InputOption.js
+++ src/InputOption.js
@@ -3,7 +3,7 @@ import './InputOption.css'
 
 function InputOption({Icon, title, color }) {
     return (
-        <div>
+        <div className="inputOption">
             <Icon style={{ color: color }}/>
             <h4>{title}</h4>
         </div>
